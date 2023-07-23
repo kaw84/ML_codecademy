87079cf18888c890effffbef970676f8e40c038e
kaw84 <128994708+kaw84@users.noreply.github.com>
2023-07-23 12:13:28 +0100

    Add files via upload

---
+++ linear_regression.py
@@ -0,0 +1,39 @@
+import codecademylib3_seaborn
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import linear_model
+
+df = pd.read_csv("https://content.codecademy.com/programs/data-science-path/linear_regression/honeyproduction.csv")
+
+print(df.head())
+
+#getting the mean
+prod_per_year = df.groupby('year').totalprod.mean()
+
+#creating new variable
+X = df['year']
+X = X.values.reshape(-1,1)
+
+y = df['totalprod']
+
+
+
+#regression
+regr = linear_model.LinearRegression()
+regr.fit(X,y)
+y_predict = regr.predict(X)
+
+#plotting raw data
+plt.scatter(X,y)
+plt.plot(X, y_predict)
+plt.show()
+
+#future prediction
+X_future = np.array(range(2013,2051))
+X_future = X_future.reshape(-1,1)
+
+future_predict = regr.predict(X_future)
+
+plt.plot(X_future, future_predict)
+plt.show()
